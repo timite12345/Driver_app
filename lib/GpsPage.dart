ec3083428e30b7b9e9774a39a38fcba3238b4885
timite12345 <timite1145@gmail.com>
2023-02-21 01:11:11 +0100

    first commit

--- lib/GpsPage.dart
+++ lib/GpsPage.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_google_places_hoc081098/flutter_google_places_hoc081098.dart';
+import 'package:google_api_headers/google_api_headers.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:google_maps_webservice/places.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -7,53 +10,95 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  String googleApikey = "AIzaSyBKAz1cjpdrP9nedtg_kmbBTGy1k_PmXX4";
   GoogleMapController? mapController; //contrller for Google map
-  Set<Marker> markers = Set(); //markers for google map
-  LatLng showLocation = LatLng(27.7089427, 85.3086209);
-  //location to show in map
-
-  @override
-  void initState() {
-    markers.add(Marker(
-      //add marker on google map
-      markerId: MarkerId(showLocation.toString()),
-      position: showLocation, //position of marker
-      infoWindow: InfoWindow(
-        //popup info
-        title: 'My Custom Title ',
-        snippet: 'My Custom Subtitle',
-      ),
-      icon: BitmapDescriptor.defaultMarker, //Icon for Marker
-    ));
-
-    //you can add more markers here
-    super.initState();
-  }
+  CameraPosition? cameraPosition;
+  LatLng startLocation = LatLng(5.316667, -4.033333);
+  String location = "Search Location";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Google Map in Flutter"),
-        backgroundColor: Colors.deepPurpleAccent,
-      ),
-      body: GoogleMap(
-        //Map widget from google_maps_flutter package
-        zoomGesturesEnabled: true, //enable Zoom in, out on map
-        initialCameraPosition: CameraPosition(
-          //innital position in map
-          target: showLocation, //initial position
-          zoom: 10.0, //initial zoom level
+        appBar: AppBar(
+          title: Text("Maps pour vous Guider"),
+          backgroundColor: Colors.deepPurpleAccent,
         ),
-        markers: markers, //markers to show on map
-        mapType: MapType.normal, //map type
-        onMapCreated: (controller) {
-          //method called when map is created
-          setState(() {
-            mapController = controller;
-          });
-        },
-      ),
-    );
+        body: Stack(children: [
+          GoogleMap(
+            //Map widget from google_maps_flutter package
+            zoomGesturesEnabled: true, //enable Zoom in, out on map
+            initialCameraPosition: CameraPosition(
+              //innital position in map
+              target: startLocation, //initial position
+              zoom: 14.0, //initial zoom level
+            ),
+            mapType: MapType.normal, //map type
+            onMapCreated: (controller) {
+              //method called when map is created
+              setState(() {
+                mapController = controller;
+              });
+            },
+          ),
+
+          //search autoconplete input
+          Positioned(
+              //search input bar
+              top: 10,
+              child: InkWell(
+                  onTap: () async {
+                    var place = await PlacesAutocomplete.show(
+                        context: context,
+                        apiKey: googleApikey,
+                        mode: Mode.overlay,
+                        types: [],
+                        strictbounds: false,
+                        components: [Component(Component.country, 'np')],
+                        //google_map_webservice package
+                        onError: (err) {
+                          print(err);
+                        });
+
+                    if (place != null) {
+                      setState(() {
+                        location = place.description.toString();
+                      });
+
+                      //form google_maps_webservice package
+                      final plist = GoogleMapsPlaces(
+                        apiKey: googleApikey,
+                        apiHeaders: await GoogleApiHeaders().getHeaders(),
+                        //from google_api_headers package
+                      );
+                      String placeid = place.placeId ?? "0";
+                      final detail = await plist.getDetailsByPlaceId(placeid);
+                      final geometry = detail.result.geometry!;
+                      final lat = geometry.location.lat;
+                      final lang = geometry.location.lng;
+                      var newlatlang = LatLng(lat, lang);
+
+                      //move map camera to selected place with animation
+                      mapController?.animateCamera(
+                          CameraUpdate.newCameraPosition(
+                              CameraPosition(target: newlatlang, zoom: 17)));
+                    }
+                  },
+                  child: Padding(
+                    padding: EdgeInsets.all(15),
+                    child: Card(
+                      child: Container(
+                          padding: EdgeInsets.all(0),
+                          width: MediaQuery.of(context).size.width - 40,
+                          child: ListTile(
+                            title: Text(
+                              location,
+                              style: TextStyle(fontSize: 18),
+                            ),
+                            trailing: Icon(Icons.search),
+                            dense: true,
+                          )),
+                    ),
+                  )))
+        ]));
   }
 }
